347ec170d8729084653336043f7e8a33b98693b2
Mete <metehannsoner@gmail.com>
2020-11-24 00:59:29 +0300

    Revert "Revert "ProductRepository added""
    This reverts commit d407554f01bb343df6c9795d71cc7185fa842ec2.

--- UdemyNLayerProject.Data/Repositories/Repository.cs
+++ UdemyNLayerProject.Data/Repositories/Repository.cs
@@ -10,10 +10,10 @@ using UdemyNLayerProject.Core.Repositories;
 namespace UdemyNLayerProject.Data.Repositories
 {
     
-    class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
-        public readonly DbContext _context;
-        public readonly DbSet<TEntity> _dbSet;
+        protected readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
 
         public Repository(DbContext context)
         {
